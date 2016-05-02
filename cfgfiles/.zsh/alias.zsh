2d0e102980b29c40dc73a7a9a42af7fb3a79ae81
Daniel chen <dchen@def-logix.com>
2016-05-02 15:35:40 -0500

    added .config/flake8, cd alias

--- cfgfiles/.zsh/alias.zsh
+++ cfgfiles/.zsh/alias.zsh
@@ -1,10 +1,3 @@
 # aliases
 alias ls='ls --color=auto'
 alias ..='cd ..'
-alias drs='python manage.py runserver'
-alias shimenv='source ~/deflogix/dshim/bin/activate'
-alias shimserv='python ~/deflogix/dshim/django-ui/manage.py runserver 0.0.0.0:8888'
-alias shimstatic='python ~/deflogix/dshim/django-ui/manage.py collectstatic'
-alias shim='python ~/deflogix/dshim/django-ui/manage.py'
-
-alias shimcopybundle='cp ~/deflogix/dshim/django-ui/dev_frontend/build/bundle* ~/deflogix/dshim/django-ui/ui/static/scripts'
